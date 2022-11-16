38e89b81dace817a9f2c823ca5edfd3fd68f4375
Maximiliano Silva <Maximiliano.Silva@endava.com>
2022-11-16 15:15:02 -0300

    haciend test de prueba

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-console.log("Bienvenidos a la DevWeek son uns cra");
+// console.log("Bienvenidos a la DevWeek son uns cra");
 
-/* function add(a, b) {
+function add(a, b) {
     return a + b;
 }
 
-module.exports = { add }; */
+module.exports = { add };
